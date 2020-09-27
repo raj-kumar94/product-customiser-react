0cec2eedce4a18474025fe3eb37a5a1525e41626
Raj Kumar <rajkmr.rkp@gmail.com>
2020-09-28 04:29:41 +0530

    Updates

---
+++ precache-manifest.0139eaaf9ab0a834c150b1ac31d25bb5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65419c27673bb1b2e96037484825bddf",
+    "url": "/customiser-react/index.html"
+  },
+  {
+    "revision": "c0be4af4ee4443120567",
+    "url": "/customiser-react/static/css/2.ec2cbdb5.chunk.css"
+  },
+  {
+    "revision": "4d1c5ab67a13d0d28f24",
+    "url": "/customiser-react/static/css/main.0ebe6272.chunk.css"
+  },
+  {
+    "revision": "c0be4af4ee4443120567",
+    "url": "/customiser-react/static/js/2.b68b55c6.chunk.js"
+  },
+  {
+    "revision": "d1f552577fbd0859131edd51f65f75d8",
+    "url": "/customiser-react/static/js/2.b68b55c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1c5ab67a13d0d28f24",
+    "url": "/customiser-react/static/js/main.554afbb0.chunk.js"
+  },
+  {
+    "revision": "9a5d006c68efdf742c29",
+    "url": "/customiser-react/static/js/runtime-main.e7615013.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/customiser-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/customiser-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/customiser-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/customiser-react/static/media/slick.f97e3bbf.svg"
+  }
+]);
